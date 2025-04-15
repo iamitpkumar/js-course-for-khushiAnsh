3b2244b62d67c03d58c45eacad298528ff5d6595
iamitpkumar <amitpkumar1893@gmail.com>
2025-04-16 00:05:11 +0530

    variables and datatypes

---
+++ 01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 1234567890;
+const accountName = "John Doe";
+let accountEmail = "amit@gmail.com";
+var accountPassword = "1234567890";
+
+console.table([accountId, accountName, accountEmail, accountPassword]);
